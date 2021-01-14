859c6617f16308248f25c203593b3b9f7f95552c
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-01-14 23:56:43 +0200

    Storing The Images On The Device

--- app/src/main/java/com/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/happyplaces/AddHappyPlaceActivity.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -21,7 +23,10 @@ import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -67,9 +72,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             }
 
         et_date.setOnClickListener(this)
-
         tv_add_image.setOnClickListener(this)
-
     }
 
     override fun onClick(v: View?) {
@@ -85,7 +88,6 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 ).show()
             }
 
-            // START
             R.id.tv_add_image -> {
                 val pictureDialog = AlertDialog.Builder(this)
                 pictureDialog.setTitle("Select Action")
@@ -102,10 +104,23 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 }
                 pictureDialog.show()
             }
-            // END
         }
     }
 
+    /**
+     * Receive the result from a previous call to
+     * {@link #startActivityForResult(Intent, int)}.  This follows the
+     * related Activity API as described there in
+     * {@link Activity#onActivityResult(int, int, Intent)}.
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode The integer result code returned by the child activity
+     *                   through its setResult().
+     * @param data An Intent, which can return result data to the caller
+     *               (various data can be attached to Intent "extras").
+     */
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
@@ -118,16 +133,30 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                         val selectedImageBitmap =
                             MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
 
+                        // TODO (Step 3 : Saving an image which is selected from GALLERY. And printed the path in logcat.)
+                        // START
+                        val saveImageToInternalStorage =
+                            saveImageToInternalStorage(selectedImageBitmap)
+                        Log.e("Saved Image : ", "Path :: $saveImageToInternalStorage")
+                        // END
+
                         iv_place_image!!.setImageBitmap(selectedImageBitmap) // Set the selected image from GALLERY to imageView.
                     } catch (e: IOException) {
                         e.printStackTrace()
                         Toast.makeText(this@AddHappyPlaceActivity, "Failed!", Toast.LENGTH_SHORT).show()
                     }
                 }
-
             } else if (requestCode == CAMERA) {
 
                 val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap // Bitmap from camera
+
+                // TODO (Step 4 : Saving an image which is selected from CAMERA. And printed the path in logcat.)
+                // START
+                val saveImageToInternalStorage =
+                    saveImageToInternalStorage(thumbnail)
+                Log.e("Saved Image : ", "Path :: $saveImageToInternalStorage")
+                // END
+
                 iv_place_image!!.setImageBitmap(thumbnail) // Set to the imageView.
             }
         } else if (resultCode == Activity.RESULT_CANCELED) {
@@ -135,40 +164,20 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-
+    /**
+     * A function to update the selected date in the UI with selected format.
+     * This function is created because every time we don't need to add format which we have added here to show it in the UI.
+     */
     private fun updateDateInView() {
         val myFormat = "dd.MM.yyyy" // mention the format you need
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault()) // A date format
         et_date.setText(sdf.format(cal.time).toString()) // A selected date using format which we have used is set to the UI.
     }
 
-    private fun takePhotoFromCamera() {
-        Dexter.withActivity(this)
-            .withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
-            )
-            .withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    // Here after all the permission are granted launch the gallery to select and image.
-                    if (report!!.areAllPermissionsGranted()) {
-                        val galleryIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        startActivityForResult(galleryIntent, CAMERA)
-                     }
-                }
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                    showRationalDialogForPermissions()
-                }
-            }).onSameThread()
-            .check()
-    }
-
+    /**
+     * A method is used for image selection from GALLERY / PHOTOS of phone storage.
+     */
     private fun choosePhotoFromGallery() {
-        // START
         Dexter.withActivity(this)
             .withPermissions(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -179,9 +188,12 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
                     // Here after all the permission are granted launch the gallery to select and image.
                     if (report!!.areAllPermissionsGranted()) {
-                        val galleryIntent = Intent(Intent.ACTION_PICK,
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
                         )
+
                         startActivityForResult(galleryIntent, GALLERY)
                     }
                 }
@@ -194,9 +206,41 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }).onSameThread()
             .check()
-        // END
     }
 
+    /**
+     * A method is used  asking the permission for camera and storage and image capturing and selection from Camera.
+     */
+    private fun takePhotoFromCamera() {
+
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    // Here after all the permission are granted launch the CAMERA to capture an image.
+                    if (report!!.areAllPermissionsGranted()) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(intent, CAMERA)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
     private fun showRationalDialogForPermissions() {
         AlertDialog.Builder(this)
             .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
@@ -211,12 +255,62 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     e.printStackTrace()
                 }
             }
-            .setNegativeButton("Cancel") { dialog, _ ->
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
                 dialog.dismiss()
             }.show()
     }
+
+    // TODO (Step 2 : Creating a method to save a copy of an selected image to internal storage for use of Happy Places App.)
+    // START
+    /**
+     * A function to save a copy of an image to internal storage for HappyPlaceApp to use.
+     */
+    private fun saveImageToInternalStorage(bitmap: Bitmap): Uri {
+
+        // Get the context wrapper instance
+        val wrapper = ContextWrapper(applicationContext)
+
+        // Initializing a new file
+        // The bellow line return a directory in internal storage
+        /**
+         * The Mode Private here is
+         * File creation mode: the default mode, where the created file can only
+         * be accessed by the calling application (or all applications sharing the
+         * same user ID).
+         */
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+
+        // Create a file to save the image
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            // Get the file output stream
+            val stream: OutputStream = FileOutputStream(file)
+
+            // Compress bitmap
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+            // Flush the stream
+            stream.flush()
+
+            // Close stream
+            stream.close()
+        } catch (e: IOException) { // Catch the exception
+            e.printStackTrace()
+        }
+
+        // Return the saved image uri
+        return Uri.parse(file.absolutePath)
+    }
+    // END
+
     companion object {
         private const val GALLERY = 1
         private const val CAMERA = 2
+        // TODO(Step 1 : Creating an cont variable to use for Directory name for copying the selected image.)
+        // START
+        private const val IMAGE_DIRECTORY = "HappyPlacesImages"
+        // END
     }
 }
